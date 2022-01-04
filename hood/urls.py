6b8dc7f41943f2cbe22c096c6d5d358fd3f21c4f
bre-nda <brenda.kanale@student.moringaschool.com>
2022-01-04 22:26:27 +0300

    add the accounts and the logout url patterns

--- hood/urls.py
+++ hood/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf.urls import url,include
+from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, logout_then_login, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('thehood/',include('thehood.urls')),
+    path('', include('thehood.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/', views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
