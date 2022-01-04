4fdb956e36a051de5c555e40a0551af54b3e11ef
bre-nda <brenda.kanale@student.moringaschool.com>
2022-01-04 22:43:02 +0300

    add the logout url pattern

--- thehood/urls.py
+++ thehood/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
+from django.contrib import admin
+from django.urls import path,re_path
 from django.contrib.auth import views as auth_views
-from . import views
-
+from thehood import views
 urlpatterns=[
-    path('',views.home,name = 'home'),]
+    path('', views.home, name = 'home'),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html')),
+    
+]
