b3133fb0054f8df6dba2fe9b87cbb93a8b38ac3a
bre-nda <brenda.kanale@student.moringaschool.com>
2022-01-05 22:45:55 +0300

    create the model profile

--- thehood/models.py
+++ thehood/models.py
@@ -1,4 +1,13 @@
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, default='')
+    image = CloudinaryField('profile-photo', null=True, transformation=[{'width':300, 'height':300}])
+    bio = models.TextField(null=True)
 
+    def __str__(self):
+        return f'{self.user.username} Profile'
