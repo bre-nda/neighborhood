2e83fbdf0570986db771c7457ccaefdbb5a75c27
bre-nda <brenda.kanale@student.moringaschool.com>
2022-01-06 17:30:53 +0300

    add the urls configuration patterns

--- thehood/urls.py
+++ thehood/urls.py
@@ -8,5 +8,11 @@ urlpatterns=[
     path('profile/', views.profile, name='profile'),
     path('hood/', views.hoods, name='hood'),
     path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
     path('new-hood/', views.new_hood, name='new-hood'),
+    path('<hood_id>/news', views.news, name='news'),                    
+    path('<hood_id>/business',views.business,name='business'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    path('businesses/<hood_id>', views.businesses, name='businesses'),
+    path('post/<hood_id>', views.post, name='post'),
 ]
