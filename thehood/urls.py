2357523dc5cf2d8e4c1311f97311040ca79710cc
bre-nda <brenda.kanale@student.moringaschool.com>
2022-01-06 13:06:23 +0300

    add urls configurations

--- thehood/urls.py
+++ thehood/urls.py
@@ -6,5 +6,7 @@ urlpatterns=[
     path('', views.home, name = 'home'),
     path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html')),
     path('profile/', views.profile, name='profile'),
-    
+    path('hood/', views.hoods, name='hood'),
+    path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
+    path('new-hood/', views.new_hood, name='new-hood'),
 ]
