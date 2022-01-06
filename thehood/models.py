93d746cf579b13f19e307dfd5a9dbd2ead709b9e
bre-nda <brenda.kanale@student.moringaschool.com>
2022-01-06 13:05:42 +0300

    create the business and post models

--- thehood/models.py
+++ thehood/models.py
@@ -11,3 +11,52 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    photo = CloudinaryField('image')
+    description = models.TextField()
+    occupants_count= models.IntegerField( default=0, blank=True)
+    health_toll =  models.IntegerField(null=True, blank=True)
+    police_toll =  models.IntegerField(null=True, blank=True)
+    def __str__(self):
+        return f'{self.name} hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
+
+class Business(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    description = models.TextField(blank=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='business')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
+
+    def __str__(self):
+        return f'{self.name} Business'
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def search_business(cls, name):
+        return cls.objects.filter(name__icontains=name).all()
+class Post(models.Model):
+    title = models.CharField(max_length=155)
+    description = models.TextField(max_length=255)
+    image = CloudinaryField('image')
+    created_at = models.DateTimeField(auto_now_add=True)
+    hood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='posting')
+    user = user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='developer')
